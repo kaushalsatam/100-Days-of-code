fd3bf0d2bef23e2803e5a689d3d9cfaa78c03160
kaushalsatam <kaushalabd17@gmail.com>
2023-12-19 13:00:55 +0530

    Day 17 added

---
+++ Day17/quiz_game/main.py
@@ -0,0 +1,19 @@
+from quiz_brain import QuizBrain
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    question_obj = Question(question_text, question_answer)
+    question_bank.append(question_obj)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+    
+print("You've completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}.")
